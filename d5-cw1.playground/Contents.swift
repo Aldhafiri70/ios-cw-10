630201f1d36cf80036c1a203c508bb5637c977a0
Aldhafiri70 <thelordshoes@Gmail.com>
2022-03-16 18:48:00 +0300

    done

--- d5-cw1.playground/Contents.swift
+++ d5-cw1.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let myfaily = ["Father", "Mother" , "Brother" , "Sister"]
+print(myfaily.count)
+var evenNumbers = [2 , 4 , 6, 8, 10, 12,]
+evenNumbers.remove(at: 2)
+evenNumbers += [16, 18, 20, 22]
+print(evenNumbers)
+
+var nums = [ 1, 2, 3, 4, 5,]
+for i in nums{
+    print(i)
+    
+}
+nums.append(6)
+nums.append(7)
+nums.append(8)
+nums.append(9)
+nums.append(10)
+print(nums)
